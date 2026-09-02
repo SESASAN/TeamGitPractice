b6e6d1986018adec262ae074eac24ed457aae478
Kevin Mendoza <kevinmendoza1111192@correo.itm.edu.co>
2026-09-02 17:08:21 -0500

    feat: agregando GET /api/members

---
+++ TeamGitPractice/Controllers/MembersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetMembers()
+    {
+        var members = new[]
+        {
+        "NOMBRE COMPLETO 1",
+        "NOMBRE COMPLETO 2",
+        "NOMBRE COMPLETO 3",
+        "NOMBRE COMPLETO 4",
+        "NOMBRE COMPLETO 5"
+        };
+        return Ok(members);
+    }
+}

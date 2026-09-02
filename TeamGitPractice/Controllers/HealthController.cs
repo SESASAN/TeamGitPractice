ba82de445b56b430031c98e69ef657a6941b6f29
NathyGaby04 <nathalie040904@gmail.com>
2026-09-02 13:24:33 -0500

    feat: agregar Health endpoint

---
+++ TeamGitPractice/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeamGitPractice.Controllers
+{
+    public class HealthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
